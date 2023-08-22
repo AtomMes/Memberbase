5e0aa487b997a9ccf11d876319fecdb30363c18a
AtomMes <atomik.2005@mail.ru>
2023-08-22 11:44:46 +0400

    hero, get-resource, resources, home responsiveness

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "./pages/home/home.component";
+import { ResourcesPageComponent } from "./pages/resources/resources-page.component";
 
 const routes: Routes = [
   {
     path: "home",
     component: HomeComponent,
+  },
+  {
+    path:'resources',
+    component:ResourcesPageComponent
   },
   { path: "", redirectTo: "home", pathMatch: "full" },
 ];
--- src/app/pages/home/components/hero/hero.component.ts
+++ src/app/pages/home/components/hero/hero.component.ts
@@ -4,19 +4,19 @@ import { Component } from "@angular/core";
   selector: "app-hero",
   template: `
     <div class="light-gradient">
-      <div class="max-w-[1200px] w-full mx-auto py-20">
-        <div class="flex items-center">
-          <div class="w-[50%] flex flex-col gap-[18px]">
-            <p class="text-[46px] font-semibold leading-tight">
+      <div class="max-w-[1200px] px-5 w-full mx-auto py-20">
+        <div class="flex flex-col-reverse md:flex-row md:items-center">
+          <div class="md:w-[50%] flex flex-col gap-[18px]">
+            <p class="text-[46px] font-semibold leading-tight max-w-[510px]">
               Access a library of free design resources
             </p>
-            <p class="text-[20px] text-[#1d1d1d] leading-tight">
+            <p class="text-[20px] text-[#1d1d1d] leading-tight max-w-[600px] ">
               The best resources and books from around the web, collected and
               curated for your reading.
             </p>
             <button class="black-button">Get Access Now</button>
           </div>
-          <div class="w-[50%] flex justify-center">
+          <div class="md:w-[50%] flex md:justify-center">
             <img src="assets/images/heroImage.svg" class="h-[320px]" />
           </div>
         </div>
